944ae829ee960a2d5fdbba6752ccd6131ad35929
Sohaib <6sohaib@gmail.com>
2019-09-29 20:49:02 +0500

    Initial Skeleton

---
+++ src/screens/Home.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import { Container, Header, Tab, Tabs, TabHeading, Icon, Text } from 'native-base';
+
+// const instructions = Platform.select({
+//   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+//   android:
+//     'Double tap R on your keyboard to reload,\n' +
+//     'Shake or press menu button for dev menu',
+// });
+// type Props = {};
+
+export default class Home extends Component<Props> {
+  render() {
+    return (
+      <Container>
+        <Header hasTabs/>
+        <Tabs>
+          <Tab heading={ <TabHeading><Icon name="camera" /><Text>Camera</Text></TabHeading>}>
+            {/* <Tab1 /> */}
+            <Text>Tab1</Text>
+          </Tab>
+          <Tab heading={ <TabHeading><Text>No Icon</Text></TabHeading>}>
+            <Text>Tab2</Text>
+          </Tab>
+          <Tab heading={ <TabHeading><Icon name="apps" /></TabHeading>}>
+            <Text>Tab3</Text>
+          </Tab>
+          <Tab heading={ <TabHeading><Icon name="apps" /></TabHeading>}>
+            <Text>Tab4</Text>
+          </Tab>
+        </Tabs>
+      </Container>
+    );
+  }
+}
